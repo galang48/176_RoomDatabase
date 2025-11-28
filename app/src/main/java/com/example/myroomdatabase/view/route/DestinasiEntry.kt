6fbc3daf013679045f26e8d4a7b3e99ebb6a81cb
galang48 <galangyudha1945@gmail.com>
2025-11-28 13:48:15 +0700

    Menambahkan EntryViewModel beserta UIStateSiswa, DetailSiswa, dan fungsi konversi untuk validasi input serta penyimpanan data Siswa.

--- app/src/main/java/com/example/myroomdatabase/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/myroomdatabase/view/route/DestinasiEntry.kt
@@ -1,7 +1,6 @@
 package com.example.myroomdatabase.view.route
 
 import com.example.myroomdatabase.R
-import com.example.myroomdatabase.view.route.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"

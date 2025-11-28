06900b23bb16358dd1052f0ebec0ae0e7173728b
galang48 <galangyudha1945@gmail.com>
2025-11-28 13:25:28 +0700

    Menambahkan interface DestinasiNavigasi sebagai kontrak route dan title untuk navigasi Compose.

--- app/src/main/java/com/example/myroomdatabase/view/route/DesninasiEntry.kt
+++ app/src/main/java/com/example/myroomdatabase/view/route/DesninasiEntry.kt
@@ -1,2 +1,9 @@
 package com.example.myroomdatabase.view.route
 
+import com.example.myroomdatabase.R
+import com.example.myroomdatabase.view.route.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}

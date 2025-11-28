f19d681447a13bdcf34bb6c27e877a0aa9500667
galang48 <galangyudha1945@gmail.com>
2025-11-28 13:30:47 +0700

    Menambahkan objek DestinasiHome sebagai implementasi DestinasiNavigasi dengan route ‘home’ dan titleRes dari app_name.

--- app/src/main/java/com/example/myroomdatabase/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/myroomdatabase/view/route/DestinasiHome.kt
@@ -1,2 +1,8 @@
 package com.example.myroomdatabase.view.route
 
+import com.example.myroomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}

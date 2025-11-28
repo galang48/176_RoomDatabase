5c191d09e62d3b0503d6247c9bfee901f2dddd6c
galang48 <galangyudha1945@gmail.com>
2025-11-28 13:58:00 +0700

    Menambahkan penyedia ViewModel (PenyediaViewModel) untuk HomeViewModel dan EntryViewModel beserta fungsi ekstensi aplikasiSiswa

--- app/src/main/java/com/example/myroomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,3 +1,30 @@
 package com.example.myroomdatabase.viewmodel.provider
 
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroomdatabase.repositori.AplikasiSiswa
+import com.example.myroomdatabase.viewmodel.EntryViewModel
+import com.example.myroomdatabase.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah
+ * instance dari [AplikasiSiswa].
+ */
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
 

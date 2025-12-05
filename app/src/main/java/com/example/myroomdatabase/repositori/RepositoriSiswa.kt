243aebe6f9c16bf79f6961e499db6ebad382ef1b
galang48 <galangyudha1945@gmail.com>
2025-12-05 09:14:29 +0700

    menambahkan fungsi get, delete, dan update di RepositoriSiswa

--- app/src/main/java/com/example/myroomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomdatabase/repositori/RepositoriSiswa.kt
@@ -7,6 +7,10 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(

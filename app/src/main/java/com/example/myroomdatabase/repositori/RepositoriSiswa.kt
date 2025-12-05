ea10a142f99624f980555762b3f59d73aaf921b1
galang48 <galangyudha1945@gmail.com>
2025-12-05 13:53:59 +0700

    Menambahkan file HalamanEdit.kt dan memformat kode di RepositoriSiswa

--- app/src/main/java/com/example/myroomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomdatabase/repositori/RepositoriSiswa.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
-
     fun getSiswaStream(id: Int): Flow<Siswa?>
     suspend fun deleteSiswa(siswa: Siswa)
     suspend fun updateSiswa(siswa: Siswa)
@@ -16,12 +15,23 @@ interface RepositoriSiswa {
 class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
 ) : RepositoriSiswa {
-    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
-    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 
-    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
-    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
-    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+    override fun getAllSiswaStream(): Flow<List<Siswa>> =
+        siswaDao.getAllSiswa()
+
+    override suspend fun insertSiswa(siswa: Siswa) =
+        siswaDao.insert(siswa)
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> =
+        siswaDao.getSiswa(id)
+
+    override suspend fun deleteSiswa(siswa: Siswa) =
+        siswaDao.delete(siswa)
+
+    override suspend fun updateSiswa(siswa: Siswa) =
+        siswaDao.update(siswa)
 }
+
+
 
 

b3210f78828e8299eb391e09494693b671713eca
galang48 <galangyudha1945@gmail.com>
2025-12-05 14:32:37 +0700

    menambahkan EditViewModel di PenyediaViewModel

--- app/src/main/java/com/example/myroomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -8,11 +8,13 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.myroomdatabase.repositori.AplikasiSiswa
 import com.example.myroomdatabase.viewmodel.DetailViewModel
+import com.example.myroomdatabase.viewmodel.EditViewModel
 import com.example.myroomdatabase.viewmodel.EntryViewModel
 import com.example.myroomdatabase.viewmodel.HomeViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+
         initializer {
             HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
@@ -20,16 +22,26 @@ object PenyediaViewModel {
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+
         initializer {
-            DetailViewModel(this.createSavedStateHandle(),
-                aplikasiSiswa().container.repositoriSiswa)
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
         }
     }
 }
 
 /**
- * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah
- * instance dari [AplikasiSiswa].
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari
+ * [AplikasiSiswa].
  */
 fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)

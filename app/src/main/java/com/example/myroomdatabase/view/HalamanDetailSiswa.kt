4a6780b23f7211d27a9b0ccbf9b201c9614438b5
galang48 <galangyudha1945@gmail.com>
2025-12-05 15:46:50 +0700

    mengaktifkan fungsionalitas edit item di halaman detail siswa

--- app/src/main/java/com/example/myroomdatabase/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/myroomdatabase/view/HalamanDetailSiswa.kt
@@ -46,11 +46,10 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailSiswaScreen(
-    //navigateToEditItem: (Int) -> Unit,
+    navigateToEditItem: (Int) -> Unit,
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory),
-    navigateToEditItem: () -> Unit
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     Scaffold(
         topBar = {
@@ -64,7 +63,7 @@ fun DetailSiswaScreen(
             val uiState = viewModel.uiDetailState.collectAsState()
             FloatingActionButton(
                 onClick = {
-                    //navigateToEditItem(uiState.value.detailSiswa.id)
+                    navigateToEditItem(uiState.value.detailSiswa.id)
                 },
                 shape = MaterialTheme.shapes.medium,
                 modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
@@ -91,6 +90,7 @@ fun DetailSiswaScreen(
         )
     }
 }
+
 @Composable
 private fun BodyDetailDataSiswa(
     detailSiswaUiState: DetailSiswaUiState,
@@ -177,6 +177,7 @@ fun DetailDataSiswa(
 
     }
 }
+
 @Composable
 private fun BarisDetailData(
     @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier

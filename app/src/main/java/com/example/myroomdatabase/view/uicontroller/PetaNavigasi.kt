470160f7df66f4eca0716c15b78b6eb37166a765
galang48 <galangyudha1945@gmail.com>
2025-12-05 10:27:53 +0700

    menambahkan kode di peta navigasi untuk detail siswa

--- app/src/main/java/com/example/myroomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myroomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -4,11 +4,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.myroomdatabase.view.DetailSiswaScreen
 import com.example.myroomdatabase.view.EntrySiswaScreen
 import com.example.myroomdatabase.view.HomeScreen
+import com.example.myroomdatabase.view.route.DestinasiDetailSiswa.itemIdArg
 import com.example.myroomdatabase.view.route.DestinasiHome
 import com.example.myroomdatabase.view.route.DestinasiEntry
 
@@ -42,6 +46,16 @@ fun HostNavigasi(
         composable(DestinasiEntry.route) {
             EntrySiswaScreen(
                 navigateBack = { navController.popBackStack() }
+            )
+        }
+        composable(DestinasiEntry.routeWithArgs,
+            arguments = listOf(navArgument(itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            DetailSiswaScreen(
+                //navigateToEditItem = { navController.navigate("${DestinasiEntry.route}/$it") },
+                navigateBack = { navController.navigateUp() }
             )
         }
     }
